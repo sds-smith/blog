46d503e1a28c06268c964cae5e002c58b0a81c25
Shawn Smith <sds.smith24@gmail.com>
2022-10-18 16:20:31 -0400

    add events array to event-bus and axios request to events array on query service startup

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,8 +5,12 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
     const event = req.body;
+
+    events.push(event);
 
     axios.post('http://localhost:4000/events', event).catch((err) => {
         console.log(err.message);
@@ -20,6 +24,10 @@ app.post('/events', (req, res) => {
     axios.post('http://localhost:4003/events', event).catch((err) => {
         console.log(err.message);
     });
+
+    app.get('/events', (req, res) => {
+        res.send(events);
+    })
 
     res.send({ status: 'OK' });
 });
