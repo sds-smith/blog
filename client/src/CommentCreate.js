856a0d745f250f316ce731451b4ee47e17b80ddb
Shawn Smith <sds.smith24@gmail.com>
2022-10-18 13:40:10 -0400

    transform get request in PostList to query service

--- client/src/CommentCreate.js
+++ client/src/CommentCreate.js
@@ -15,7 +15,7 @@ const CommentCreate = ({postId}) => {
     return (
         <div>
             <form onSubmit={onSubmit} >
-                <div classMane="form-group">
+                <div className="form-group">
                     <label>New Comment</label>
                     <input value={content} onChange={e => setContent(e.target.value)} className='form-control'/>
                 </div>
